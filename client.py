58d97bb0c6ca6d799c86cbe54db40c05df4509b2
nbenvenuto <natale.benvenutogreco@gmail.com>
2021-03-04 09:40:35 +0100

    "Set Serial Number" implemented

--- client.py
+++ client.py
@@ -1,20 +1,43 @@
 import socket
 import time
 
+# Variables definitions
+ip_working = "192.168.0.201"
+port = 20000
+SerialNumber = "B18P03933"
 
-# create socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect to server
-clientSocket.connect(("192.168.0.10", 20000))
+clientSocket.connect((ip_working, port))
 
-# send data to server
-data = "<ESC>GetWindowSerial<CR>"
-clientSocket.send(data.encode())
+# receive data to server
+dataFromServer = clientSocket.recv(1024)
+message_decoded = dataFromServer.decode(encoding="ascii")
 
+# print to console the received message
+print("Versione PTS" + " " + message_decoded)
+
+
+# write the message to send the server (in this step it is important to convert the characters ESC, LF, CR in x1b,x0a,r)
+Set_SerialNumber = '\x1b' + "SetSerialNumber" +'\x0a' + SerialNumber + '\r'
+Get_SerialNumber = '\x1b' + "GetSerialNumber" + '\r'
+
+# send Set_SerialNumber to server
+clientSocket.send(Set_SerialNumber.encode())
+time.sleep(2)
 # receive message from server
-#dataFromServer = clientSocket.recv(1024)
-#message_decoded = dataFromServer.decode()
+dataFromServer = clientSocket.recv(1024)
+message_decoded = dataFromServer.decode()
+print("Stato operazione" + " " + message_decoded)
 
-# print to console
-#print(message_decoded)
+# send Get_SerialNumber to server
+clientSocket.send(Get_SerialNumber.encode())
+# receive message from server
+dataFromServer = clientSocket.recv(1024)
+message_decoded = dataFromServer.decode()
+print("The New" + " " + message_decoded)
+
+# close the socket connection
+clientSocket.close()
+
